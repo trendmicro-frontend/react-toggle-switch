95e7c6065ba5ad9e92a1469643bb5bfdfebb57a6
Tushar Agarwal <niftytushar@gmail.com>
2019-10-01 11:13:35 +0530

    Add props to customize on/off colors

--- examples/index.jsx
+++ examples/index.jsx
@@ -102,6 +102,59 @@ class App extends React.Component {
                         </div>
                     </div>
                 </div>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <div className="panel-title">
+                                    Colored
+                                </div>
+                            </div>
+                            <div className="panel-body">
+                                <div className="container-fluid">
+                                    <div className="row">
+                                        <div className="col-sm-6" style={{ minHeight: 40, padding: '8px 0' }}>
+                                            <ToggleSwitch
+                                                onColor="green"
+                                                checked
+                                                size="lg"
+                                            />
+                                            ON Green
+                                        </div>
+                                        <div className="col-sm-6" style={{ minHeight: 40, padding: '8px 0' }}>
+                                            <ToggleSwitch
+                                                onColor="green"
+                                                disabled
+                                                checked
+                                                size="lg"
+                                            />
+                                            ON Green (Disabled)
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-sm-6" style={{ minHeight: 40, padding: '8px 0' }}>
+                                            <ToggleSwitch
+                                                offColor="red"
+                                                checked={false}
+                                                size="lg"
+                                            />
+                                            OFF Red
+                                        </div>
+                                        <div className="col-sm-6" style={{ minHeight: 40, padding: '8px 0' }}>
+                                            <ToggleSwitch
+                                                onColor="green"
+                                                offColor="red"
+                                                checked
+                                                size="lg"
+                                            />
+                                            ON / OFF (Red / Green)
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
